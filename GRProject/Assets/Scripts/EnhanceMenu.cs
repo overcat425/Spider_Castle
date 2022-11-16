3dc3c9d347d7c05fbfc8e4f9b1d07ef4a3c5a8ff
overcat425 <105467409+overcat425@users.noreply.github.com>
2022-11-16 12:39:51 +0900

    EnhanceTab UI Amended
    22/11/16
    Skill Buttons added (+Actions)
    Skill Summary added

--- GRProject/Assets/Scripts/EnhanceMenu.cs
+++ GRProject/Assets/Scripts/EnhanceMenu.cs
@@ -10,6 +10,15 @@ public class EnhanceMenu : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
     [SerializeField]    private GameObject LabTab;
     [SerializeField]    private GameObject EnhanceTab;
     [SerializeField]    private GameObject CobTab;
+    [SerializeField]    private GameObject Summary;
+
+    [Header("½ºÅ³")]
+    [SerializeField] private GameObject Skill1;
+    [SerializeField] private GameObject Skill2;
+    [SerializeField] private GameObject Skill3;
+    [SerializeField] private GameObject Skill4;
+    [SerializeField] private GameObject Skill5;
+    [SerializeField] private GameObject Skill6;
 
     public Transform buttonScale;
     Vector3 defaultScale;
@@ -44,12 +53,73 @@ public class EnhanceMenu : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
         LabTab.SetActive(false);
         EnhanceTab.SetActive(true);
         CobTab.SetActive(false);
+        Summary.SetActive(false);
     }
     public void OnClickCobTab()
     {
         LabTab.SetActive(false);
         EnhanceTab.SetActive(false);
         CobTab.SetActive(true);
+    }
+    public void OnClickSkill1Icon()
+    {
+        Summary.SetActive(true);
+        Skill1.SetActive(true);
+        Skill2.SetActive(false);
+        Skill3.SetActive(false);
+        Skill4.SetActive(false);
+        Skill5.SetActive(false);
+        Skill6.SetActive(false);
+    }
+    public void OnClickSkill2Icon()
+    {
+        Summary.SetActive(true);
+        Skill2.SetActive(true);
+        Skill1.SetActive(false);
+        Skill3.SetActive(false);
+        Skill4.SetActive(false);
+        Skill5.SetActive(false);
+        Skill6.SetActive(false);
+    }
+    public void OnClickSkill3Icon()
+    {
+        Summary.SetActive(true);
+        Skill3.SetActive(true);
+        Skill1.SetActive(false);
+        Skill2.SetActive(false);
+        Skill4.SetActive(false);
+        Skill5.SetActive(false);
+        Skill6.SetActive(false);
+    }
+    public void OnClickSkill4Icon()
+    {
+        Summary.SetActive(true);
+        Skill4.SetActive(true);
+        Skill1.SetActive(false);
+        Skill2.SetActive(false);
+        Skill3.SetActive(false);
+        Skill5.SetActive(false);
+        Skill6.SetActive(false);
+    }
+    public void OnClickSkill5Icon()
+    {
+        Summary.SetActive(true);
+        Skill5.SetActive(true);
+        Skill1.SetActive(false);
+        Skill2.SetActive(false);
+        Skill3.SetActive(false);
+        Skill4.SetActive(false);
+        Skill6.SetActive(false);
+    }
+    public void OnClickSkill6Icon()
+    {
+        Summary.SetActive(true);
+        Skill6.SetActive(true);
+        Skill1.SetActive(false);
+        Skill2.SetActive(false);
+        Skill3.SetActive(false);
+        Skill4.SetActive(false);
+        Skill5.SetActive(false);
     }
     public void OnPointerEnter(PointerEventData eventData)
     {
